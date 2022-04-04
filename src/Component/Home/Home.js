591e8db09e07e450b23b1a47065c517b9322150a
Iftekhar Utsho <future.slangsta76@gmail.com>
2022-04-04 14:12:47 +0600

    route installed

---
+++ src/Component/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <img src='https://ibb.co/ZJH1qXy' alt="" />
+        </div>
+    );
+};
+
+export default Home;
